791d88daaf8b129b09455c0cdeaae93592f746e7
HarryZ7G <56236602+HarryZ7G@users.noreply.github.com>
2023-06-08 18:55:45 -0400

    Development restarted

--- README.md
+++
@@ -1,1 +0,0 @@
-## This is still a work in progress but you can check it out [here](https://harryz7g.github.io/my-site/) :)
--- src/App.js
+++ src/App.js
@@ -1,42 +1,66 @@
-import React from 'react';
-import About from './Components/About/index.js';
-import Frost from './Components/FrostedGlass/index.js';
-import ProfileImg from './Components/ProfileImg/index.js';
-import RefreshMsg from './Components/RefreshMsg/index.js';
-import ScrollMsg from './Components/ScrollMsg/index.js';
-import Skills from './Components/Skills/index.js';
-import 'animate.css';
-import './App.css'
+import React, { useEffect } from "react";
+import "./App.css";
 
-class App extends React.Component {
+function App() {
+  const [circle, setCircle] = React.useState(null);
+  const [disable, setDisable] = React.useState(null);
+  const [progress, setProgress] = React.useState(null);
+  const [fill, setFill] = React.useState(null);
+  // const [background, setBackground] = React.useState(null);
+  const [circles, setCircles] = React.useState(["", "", "", "", "", ""]);
 
-  componentDidMount = () => {
-    window.addEventListener('scroll', function(){
-      if (window.pageYOffset < window.innerHeight / 2) {
-        document.getElementById("back").style.filter = `blur(${window.pageYOffset / (window.innerHeight/2) * 15}px)`;
-      } else {
-        document.getElementById("back").style.filter = "blur(15px)";
-      }
-    });
-  };
+  useEffect(() => {
+    setTimeout(() => {
+      setCircle("animate-circle");
+    }, 1000);
+  }, []);
 
-  render() {
-    return (
-      <div className="root" id="root">
-        <div className="background animate__animated animate__fadeIn animate__delay-1s" id="back"></div>
-        <div className="title-back">
-          <RefreshMsg/>
-          <Frost/>
-          <ScrollMsg/>
+  return (
+    <div className="root">
+      <div className={`landing ${circles[0]}`}>
+        <div
+          className={`circle ${circle} ${disable}`}
+          onClick={() => {
+            setProgress("animate-progress");
+            setFill("animate-fill");
+            setTimeout(() => {
+              setDisable("disabled");
+            }, 100);
+            setTimeout(() => {
+              setCircles([
+                "blue",
+                "shadow-appear",
+                "tall-shadow-appear",
+                "inset-shadow-appear",
+                "tall-inset-shadow-appear",
+                "text-shadow",
+              ]);
+            }, 5000);
+          }}
+        />
+        <div className="progress-container">
+          <div className={`progress ${progress}`} />
+          <div className={`fill ${fill}`} />
         </div>
-        <div className="intro animate__animated animate__fadeIn animate__delay-2s">
-          <ProfileImg/>
-          <About/>
-          <Skills/>
+        <div className={`top-outer-circle ${circles[2]}`}>
+          <div className={`top-middle-circle ${circles[3]}`}>
+            <div className={`top-inner-circle ${circles[3]}`}>
+              <div className={`top-center-outer-circle ${circles[2]}`}>
+                <div className={`top-center-middle-circle ${circles[2]}`}>
+                  <div className={`top-center-inner-circle ${circles[4]}`} />
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className={`thick-dash ${circles[1]}`} />
+        <div className={`thin-dash ${circles[2]}`} />
+        <div className={`name-plate ${circles[3]}`}>
+          <h1 className={`title ${circles[5]}`}>Hi, I'm Harry</h1>
         </div>
       </div>
-    );
-  }
+    </div>
+  );
 }
 
 export default App;
